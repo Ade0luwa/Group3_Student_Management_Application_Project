3cb8a1aed2c5f70c9aaaa3f1bb7a3eb67907aaf9
USER-PC <USER-PC@ANGEL>
2024-04-15 01:04:29 -0400

    Design for other students boards - Courses, Grades, and professor boards - Dashboard and notification

--- Group3_Student_Management_Application/StudentNotification.Designer.cs
+++ Group3_Student_Management_Application/StudentNotification.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StudentNotification));
+            this.notification_label = new System.Windows.Forms.Label();
+            this.logout_label = new System.Windows.Forms.Label();
+            this.grades_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.courses_label = new System.Windows.Forms.Label();
+            this.dashboard_label = new System.Windows.Forms.Label();
+            this.math_label = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notification_label
+            // 
+            this.notification_label.AutoSize = true;
+            this.notification_label.BackColor = System.Drawing.Color.Honeydew;
+            this.notification_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.notification_label.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notification_label.Location = new System.Drawing.Point(19, 411);
+            this.notification_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.notification_label.Name = "notification_label";
+            this.notification_label.Size = new System.Drawing.Size(158, 32);
+            this.notification_label.TabIndex = 19;
+            this.notification_label.Text = "Notification";
+            // 
+            // logout_label
+            // 
+            this.logout_label.AutoSize = true;
+            this.logout_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.logout_label.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logout_label.Location = new System.Drawing.Point(19, 478);
+            this.logout_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.logout_label.Name = "logout_label";
+            this.logout_label.Size = new System.Drawing.Size(99, 32);
+            this.logout_label.TabIndex = 11;
+            this.logout_label.Text = "Logout";
+            // 
+            // grades_label
+            // 
+            this.grades_label.AutoSize = true;
+            this.grades_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.grades_label.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grades_label.Location = new System.Drawing.Point(19, 341);
+            this.grades_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.grades_label.Name = "grades_label";
+            this.grades_label.Size = new System.Drawing.Size(99, 32);
+            this.grades_label.TabIndex = 18;
+            this.grades_label.Text = "Grades";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 180);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // courses_label
+            // 
+            this.courses_label.AutoSize = true;
+            this.courses_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.courses_label.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.courses_label.Location = new System.Drawing.Point(19, 278);
+            this.courses_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.courses_label.Name = "courses_label";
+            this.courses_label.Size = new System.Drawing.Size(110, 32);
+            this.courses_label.TabIndex = 15;
+            this.courses_label.Text = "Courses";
+            // 
+            // dashboard_label
+            // 
+            this.dashboard_label.AutoSize = true;
+            this.dashboard_label.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.dashboard_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dashboard_label.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.dashboard_label.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_label.Location = new System.Drawing.Point(19, 214);
+            this.dashboard_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.dashboard_label.Name = "dashboard_label";
+            this.dashboard_label.Size = new System.Drawing.Size(148, 32);
+            this.dashboard_label.TabIndex = 16;
+            this.dashboard_label.Text = "Dashboard";
+            // 
+            // math_label
+            // 
+            this.math_label.AutoSize = true;
+            this.math_label.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.math_label.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.math_label.Location = new System.Drawing.Point(279, 216);
+            this.math_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.math_label.Name = "math_label";
+            this.math_label.Size = new System.Drawing.Size(754, 57);
+            this.math_label.TabIndex = 25;
+            this.math_label.Text = "Rename to Professor Notification";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.notification_label);
+            this.panel1.Controls.Add(this.logout_label);
+            this.panel1.Controls.Add(this.grades_label);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.courses_label);
+            this.panel1.Controls.Add(this.dashboard_label);
+            this.panel1.Location = new System.Drawing.Point(21, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(196, 538);
+            this.panel1.TabIndex = 24;
+            // 
+            // StudentNotification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(1086, 582);
+            this.Controls.Add(this.math_label);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StudentNotification";
             this.Text = "StudentNotification";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label notification_label;
+        private System.Windows.Forms.Label logout_label;
+        private System.Windows.Forms.Label grades_label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label courses_label;
+        private System.Windows.Forms.Label dashboard_label;
+        private System.Windows.Forms.Label math_label;
+        private System.Windows.Forms.Panel panel1;
     }
 }
