a3cde66a10e18c059cc1e103583a1782b2f4b0f0
USER-PC <USER-PC@ANGEL>
2024-04-14 20:21:10 -0400

    Student Dashboard design

--- Group3_Student_Management_Application/SignUp.Designer.cs
+++ Group3_Student_Management_Application/SignUp.Designer.cs
@@ -72,9 +72,10 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(973, 625);
+            this.panel1.Size = new System.Drawing.Size(649, 406);
             this.panel1.TabIndex = 1;
             // 
             // signUp_close
@@ -82,9 +83,10 @@
             this.signUp_close.AutoSize = true;
             this.signUp_close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.signUp_close.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_close.Location = new System.Drawing.Point(891, 26);
+            this.signUp_close.Location = new System.Drawing.Point(594, 17);
+            this.signUp_close.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.signUp_close.Name = "signUp_close";
-            this.signUp_close.Size = new System.Drawing.Size(27, 28);
+            this.signUp_close.Size = new System.Drawing.Size(19, 19);
             this.signUp_close.TabIndex = 22;
             this.signUp_close.Text = "X";
             this.signUp_close.Click += new System.EventHandler(this.signUp_close_Click);
@@ -94,9 +96,10 @@
             this.signUp_loginHere.AutoSize = true;
             this.signUp_loginHere.Cursor = System.Windows.Forms.Cursors.Hand;
             this.signUp_loginHere.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_loginHere.Location = new System.Drawing.Point(799, 575);
+            this.signUp_loginHere.Location = new System.Drawing.Point(533, 374);
+            this.signUp_loginHere.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.signUp_loginHere.Name = "signUp_loginHere";
-            this.signUp_loginHere.Size = new System.Drawing.Size(105, 23);
+            this.signUp_loginHere.Size = new System.Drawing.Size(74, 16);
             this.signUp_loginHere.TabIndex = 21;
             this.signUp_loginHere.Text = "Login Here";
             this.signUp_loginHere.Click += new System.EventHandler(this.signUp_loginHere_Click);
@@ -105,86 +108,95 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(558, 575);
+            this.label5.Location = new System.Drawing.Point(372, 374);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(226, 23);
+            this.label5.Size = new System.Drawing.Size(161, 16);
             this.label5.TabIndex = 20;
             this.label5.Text = "Already have an account?";
             // 
             // signUp_phone
             // 
             this.signUp_phone.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_phone.Location = new System.Drawing.Point(414, 469);
+            this.signUp_phone.Location = new System.Drawing.Point(276, 305);
+            this.signUp_phone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_phone.Multiline = true;
             this.signUp_phone.Name = "signUp_phone";
-            this.signUp_phone.Size = new System.Drawing.Size(490, 37);
+            this.signUp_phone.Size = new System.Drawing.Size(328, 25);
             this.signUp_phone.TabIndex = 19;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(409, 440);
+            this.label4.Location = new System.Drawing.Point(273, 286);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(165, 26);
+            this.label4.Size = new System.Drawing.Size(111, 17);
             this.label4.TabIndex = 18;
             this.label4.Text = "Phone Number";
             // 
             // signUp_password
             // 
             this.signUp_password.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_password.Location = new System.Drawing.Point(414, 387);
+            this.signUp_password.Location = new System.Drawing.Point(276, 252);
+            this.signUp_password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_password.Multiline = true;
             this.signUp_password.Name = "signUp_password";
             this.signUp_password.PasswordChar = '*';
-            this.signUp_password.Size = new System.Drawing.Size(490, 37);
+            this.signUp_password.Size = new System.Drawing.Size(328, 25);
             this.signUp_password.TabIndex = 17;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(409, 358);
+            this.label8.Location = new System.Drawing.Point(273, 233);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(110, 26);
+            this.label8.Size = new System.Drawing.Size(76, 17);
             this.label8.TabIndex = 16;
             this.label8.Text = "Password";
             // 
             // signUp_email
             // 
             this.signUp_email.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_email.Location = new System.Drawing.Point(414, 308);
+            this.signUp_email.Location = new System.Drawing.Point(276, 200);
+            this.signUp_email.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_email.Multiline = true;
             this.signUp_email.Name = "signUp_email";
-            this.signUp_email.Size = new System.Drawing.Size(490, 37);
+            this.signUp_email.Size = new System.Drawing.Size(328, 25);
             this.signUp_email.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(409, 279);
+            this.label3.Location = new System.Drawing.Point(273, 181);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 26);
+            this.label3.Size = new System.Drawing.Size(108, 17);
             this.label3.TabIndex = 14;
             this.label3.Text = "Email Address";
             // 
             // signUp_lastName
             // 
             this.signUp_lastName.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_lastName.Location = new System.Drawing.Point(414, 230);
+            this.signUp_lastName.Location = new System.Drawing.Point(276, 150);
+            this.signUp_lastName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_lastName.Multiline = true;
             this.signUp_lastName.Name = "signUp_lastName";
-            this.signUp_lastName.Size = new System.Drawing.Size(490, 37);
+            this.signUp_lastName.Size = new System.Drawing.Size(328, 25);
             this.signUp_lastName.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(409, 201);
+            this.label7.Location = new System.Drawing.Point(273, 131);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(118, 26);
+            this.label7.Size = new System.Drawing.Size(78, 17);
             this.label7.TabIndex = 12;
             this.label7.Text = "Last Name";
             // 
@@ -192,9 +204,10 @@
             // 
             this.signUp_prof.AutoSize = true;
             this.signUp_prof.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_prof.Location = new System.Drawing.Point(546, 66);
+            this.signUp_prof.Location = new System.Drawing.Point(364, 43);
+            this.signUp_prof.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_prof.Name = "signUp_prof";
-            this.signUp_prof.Size = new System.Drawing.Size(118, 27);
+            this.signUp_prof.Size = new System.Drawing.Size(82, 20);
             this.signUp_prof.TabIndex = 11;
             this.signUp_prof.TabStop = true;
             this.signUp_prof.Text = "Professor";
@@ -204,9 +217,10 @@
             // 
             this.signUp_student.AutoSize = true;
             this.signUp_student.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_student.Location = new System.Drawing.Point(414, 66);
+            this.signUp_student.Location = new System.Drawing.Point(276, 43);
+            this.signUp_student.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_student.Name = "signUp_student";
-            this.signUp_student.Size = new System.Drawing.Size(102, 27);
+            this.signUp_student.Size = new System.Drawing.Size(73, 20);
             this.signUp_student.TabIndex = 10;
             this.signUp_student.TabStop = true;
             this.signUp_student.Text = "Student";
@@ -216,9 +230,10 @@
             // 
             this.signUp_btn.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.signUp_btn.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_btn.Location = new System.Drawing.Point(416, 528);
+            this.signUp_btn.Location = new System.Drawing.Point(277, 343);
+            this.signUp_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_btn.Name = "signUp_btn";
-            this.signUp_btn.Size = new System.Drawing.Size(116, 52);
+            this.signUp_btn.Size = new System.Drawing.Size(77, 34);
             this.signUp_btn.TabIndex = 6;
             this.signUp_btn.Text = "SUBMIT";
             this.signUp_btn.UseVisualStyleBackColor = false;
@@ -227,19 +242,21 @@
             // signUp_firstName
             // 
             this.signUp_firstName.Font = new System.Drawing.Font("Cambria", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.signUp_firstName.Location = new System.Drawing.Point(414, 150);
+            this.signUp_firstName.Location = new System.Drawing.Point(276, 98);
+            this.signUp_firstName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.signUp_firstName.Multiline = true;
             this.signUp_firstName.Name = "signUp_firstName";
-            this.signUp_firstName.Size = new System.Drawing.Size(490, 37);
+            this.signUp_firstName.Size = new System.Drawing.Size(328, 25);
             this.signUp_firstName.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(409, 121);
+            this.label2.Location = new System.Drawing.Point(273, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 26);
+            this.label2.Size = new System.Drawing.Size(82, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "First Name";
             // 
@@ -247,9 +264,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cambria", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(408, 21);
+            this.label1.Location = new System.Drawing.Point(272, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 33);
+            this.label1.Size = new System.Drawing.Size(75, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Sign Up";
             // 
@@ -258,27 +276,30 @@
             this.panel2.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.panel2.Controls.Add(this.label6);
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(402, 615);
+            this.panel2.Size = new System.Drawing.Size(268, 400);
             this.panel2.TabIndex = 0;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 298);
+            this.label6.Location = new System.Drawing.Point(15, 194);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(362, 21);
+            this.label6.Size = new System.Drawing.Size(235, 14);
             this.label6.TabIndex = 2;
             this.label6.Text = "Group 3 Student Management Application";
             // 
             // SignUp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 615);
+            this.ClientSize = new System.Drawing.Size(633, 400);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignUp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SignUp";
